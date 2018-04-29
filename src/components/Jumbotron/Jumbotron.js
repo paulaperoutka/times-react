33785912bd3d436489294ec07f560c58f72b753d
paulaperoutka <paulaperoutka@gmail.com>
2018-04-29 16:32:40 -0400

    jumbotron rendering

--- src/components/Jumbotron/Jumbotron.js
+++ src/components/Jumbotron/Jumbotron.js
@@ -2,7 +2,7 @@ import React from "react";
 
 const Jumbotron = () => (
 	<div className="jumbotron">
-		<div className="container text-center">
+		<div className="container">
 			<h1>New York Times</h1>
 			<h2>Article Scraper</h2>
 			<h3>Search for, save, and annotate New York Times articles.</h3>
